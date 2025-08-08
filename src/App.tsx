6a851636b122a955d6dca1dbf790ebebb3738767
ahad1033 <a.ahad2232@gmail.com>
2025-08-08 17:41:10 +0600

    Initial setup with tailwindcss and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500">Skill Certify</h1>
+
+      <Button variant="destructive">Click Here</Button>
+    </>
+  );
+}
+
+export default App;
